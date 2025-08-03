9db76c6bbdee99a2ab31968c6bbe51213c32837e
Eugene <es.newone@gmail.com>
2025-08-03 11:51:15 +0300

    code word modified by random

--- sketch.js
+++ sketch.js
@@ -11,17 +11,25 @@ function setup() {
   noStroke();
 
   points = font.textToPoints('c o d e', 50, 300, 300, {
-    sampleFactor: .3,
+    sampleFactor: .1,
     simplifyThreshold: 0
   });
+
 
 }
 
 function draw() {
-    background(0);
+  background(0);
 
-    // *** your code here ****
+  let m = mouseX;
+  var r = map(m, 0, width, 0, 100);
 
+  for (let p of points){
+    let x = random(p.x-r, p.x+r);
+    let y = random(p.y-r, p.y+r);
+
+    ellipse(x, y, 7, 7);
+  }
     noLoop();
 }
 
